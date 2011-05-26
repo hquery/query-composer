1912aab18bf425c9d64ebde35bc776b1ff39de82
Marc Hadley <mhadley@mitre.org>
2011-05-26 09:36:44 -0400

    Initial clean rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QueryComposer::Application.config.secret_token = '6233995480025457914a037966e3d4485e48629c6724198b528fa2c8256a8efc8dc1be29a11be27b634d38dd61a230ee0ad51eee06a828992f6594061540adb6'
