fea6516d05bbc953d74d75616bb733cbc240420e
Andre Quina <aquina@mitre.org>
2011-06-28 18:21:55 -0400

    removed pry from development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,12 +26,12 @@ QueryComposer::Application.configure do
   config.assets.compress = false
 
   # add support for Pry debugging
- silence_warnings do
-   begin
-     require 'pry'
-     IRB = Pry
-   rescue LoadError
-   end
- end
+ # silence_warnings do
+ #   begin
+ #     require 'pry'
+ #     IRB = Pry
+ #   rescue LoadError
+ #   end
+ # end
 
 end
