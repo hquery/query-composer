d6d3b79fc27e91461c3f2a89098390126553b5cc
Andre Quina <aquina@mitre.org>
2011-07-01 16:03:34 -0400

    updated tests to increase coverage

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -3,6 +3,8 @@ class Endpoint
 
   has_and_belongs_to_many :queries
   
+  validates_presence_of :name, :submit_url
+  
   field :name, type: String
   field :submit_url, type: String
 end
--- test/functional/endpoints_controller_test.rb
+++ test/functional/endpoints_controller_test.rb
@@ -33,6 +33,28 @@ class EndpointsControllerTest < ActionController::TestCase
     assert_redirected_to endpoint_path(assigns(:endpoint))
   end
 
+  test "should not create endpoint without required fields" do
+    # make sure that submit_url is required
+    post :create, endpoint: {name: "blah"}
+    invalid_endpoint = assigns[:endpoint]
+    assert_not_nil invalid_endpoint
+    assert !invalid_endpoint.valid?
+    assert invalid_endpoint.errors[:name].empty?
+    assert !invalid_endpoint.errors[:submit_url].empty?
+    assert_response :success
+    
+  end
+  
+  test "endpoint name should be required" do
+    #make sure that name is also required
+    post :create, endpoint: {submit_url: "blah"}
+    invalid_endpoint = assigns[:endpoint]
+    assert !invalid_endpoint.valid?
+    assert !invalid_endpoint.errors[:name].empty?
+    assert invalid_endpoint.errors[:submit_url].empty?
+    assert_response :success
+  end
+
 
 
   test "should show endpoint" do
@@ -49,6 +71,20 @@ class EndpointsControllerTest < ActionController::TestCase
     put :update, id: @endpoint.to_param, endpoint: @endpoint.attributes
     assert_redirected_to endpoint_path(assigns(:endpoint))
   end
+
+  test "should not update endpoint if invalid" do
+    # make sure we can't update to be invalid
+    put :update, id: @endpoint.to_param, endpoint: {submit_url: nil}
+    invalid_endpoint = assigns[:endpoint]
+    assert_not_nil invalid_endpoint
+    assert !invalid_endpoint.valid?
+    assert invalid_endpoint.errors[:name].empty?
+    assert !invalid_endpoint.errors[:submit_url].empty?
+    assert_response :success
+
+  end
+  
+  
 
   test "should destroy endpoint" do
     assert_difference('Endpoint.count', -1) do
