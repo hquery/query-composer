0779719c5569b93d892977536c3f5d5143b5d938
Adam Goldstein <agoldstein@mitre.org>
2011-07-18 09:42:26 -0400

    Address story 15603051, /endpoints now shows the status of all listed endpoints. I'm also adding a couple of relevant functional tests.

--- app/helpers/endpoints_helper.rb
+++ app/helpers/endpoints_helper.rb
@@ -1,2 +1,27 @@
 module EndpointsHelper
+  def fetch_endpoint_statuses
+    @endpoint_server_statuses = {}
+    @endpoints.each do |endpoint|
+      url = URI.parse endpoint.submit_url
+      request = Net::HTTP::Get.new(url.path + "/server_status")
+      begin
+        Net::HTTP.start(url.host, url.port) do |http|
+          response = http.request(request)
+          if Net::HTTPSuccess
+            @endpoint_server_statuses[endpoint.id] = JSON.parse(response.body)
+          end
+        end
+      rescue Exception => ex
+        @endpoint_server_statuses[endpoint.id] = {
+          'queued' => 'unknown',
+          'running' => 'unknown',
+          'successful' => 'unknown',
+          'failed' => 'unknown',
+          'retried' => 'unknown',
+          'avg_runtime' => 'unknown',
+          'backend_status' => 'unreachable'
+        }
+      end
+    end
+  end
 end
