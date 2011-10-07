db8c3b5084c9a02441b7767e79a907720a92f196
Andy Gregorowicz <andy@mitre.org>
2011-10-07 14:27:12 -0400

    Updating the GeneratedResultPresenter to deal with new result structure. Hardcoding some things for nice graphs now. Will need to refactor later.

--- lib/generated_result_presenter.rb
+++ lib/generated_result_presenter.rb
@@ -3,40 +3,15 @@ class GeneratedResultPresenter < ResultPresenter
 
   def initialize(title, result_hash)
     super(title, result_hash)
-    @group_order = []
-    @grouped_values = {}
-    groupify
-  end
 
-  def key_javascript_array
-    @grouped_values.keys.to_json
-  end
-
-  def value_javascript_array
-    @grouped_values.values.to_json
-  end
-
-  private
-  def population_free_each_pair
-    each_pair do |key, value|
-      next if key.eql? 'Populations'
-      yield key, value
-    end
-  end
-
-  # Populates the group_order and grouped_values instance variables
-  # grouped_values is a hash. The key will be an attribute of a patient 
-  # and its grouping, such as "age_mean".
-  # The value will be an array of values for that group. Following from the example
-  # above, age_mean would contain a list of mean ages 
-  def groupify
-    population_free_each_pair do |population_group, value|
-      @group_order << population_group
-      value.each_pair do |population_attribute, inner_value|
-        inner_value.each_pair do |math_function, resulting_value|
-          @grouped_values[population_attribute + '_' + math_function] ||= []
-          @grouped_values[population_attribute + '_' + math_function] << resulting_value
-        end
+    if result_hash.present?
+      # Reset the work done in super
+      @keys = []
+      @values = []
+      # Ugly hack for now
+      result_hash['Results'].values.first.values.first.each_pair do |key, value|
+        @keys << key
+        @values << value
       end
     end
   end
