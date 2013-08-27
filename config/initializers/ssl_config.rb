db668d69983e5bfe2b05f17ae1ba4213b0a6dd15
Raymond Rusk <rusk.raymond@gmail.com>
2013-08-27 14:44:45 -0700

    Reconstruct missing openssl root certificates

--- config/initializers/ssl_config.rb
+++ config/initializers/ssl_config.rb
@@ -1,9 +1,9 @@
 #contains constants for certificate used for SSL
-SERVER_CERT_PATH_ = "cert/composer.crt"
 SERVER_KEY_PATH_ = "cert/composer.key"
+SERVER_CERT_PATH_ = "cert/composer.crt"
 
 CLIENT_KEY_PATH = "cert/client.key"
-CLIENT_CERT_PATH = "cert/client.pem"
+CLIENT_CERT_PATH = "cert/client.crt"
 
 SERVER_KEY = OpenSSL::PKey::RSA.new(File.open(SERVER_KEY_PATH_).read)
 SERVER_CERT = OpenSSL::X509::Certificate.new(File.open(SERVER_CERT_PATH_).read)
