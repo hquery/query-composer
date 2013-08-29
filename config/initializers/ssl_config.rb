900bbd37854be395412b3eb1e5199fd266232e67
Raymond Rusk <rusk.raymond@gmail.com>
2013-08-29 13:16:56 -0700

    Updated openssl self-signed certificates and documentation.

--- config/initializers/ssl_config.rb
+++ config/initializers/ssl_config.rb
@@ -1,12 +1,16 @@
 #contains constants for certificate used for SSL
-SERVER_KEY_PATH_ = "cert/composer.key"
-SERVER_CERT_PATH_ = "cert/composer.crt"
-
-CLIENT_KEY_PATH = "cert/client.key"
-CLIENT_CERT_PATH = "cert/client.crt"
-
+#SERVER_KEY_PATH_ = "cert/composer.key"
+#SERVER_CERT_PATH_ = "cert/composer.crt"
+SERVER_KEY_PATH_ = "cert/ca/LeadLab_root_cert_TEST.pem"
+SERVER_CERT_PATH_ = "cert/ca/LeadLab_root_cert_TEST.pem"
+#
+#CLIENT_KEY_PATH = "cert/client.key"
+#CLIENT_CERT_PATH = "cert/client.crt"
+CLIENT_KEY_PATH = "cert/ca/LeadLab_root_cert_TEST.pem"
+CLIENT_CERT_PATH = "cert/ca/LeadLab_root_cert_TEST.pem"
+#
 SERVER_KEY = OpenSSL::PKey::RSA.new(File.open(SERVER_KEY_PATH_).read)
 SERVER_CERT = OpenSSL::X509::Certificate.new(File.open(SERVER_CERT_PATH_).read)
-
+#
 CLIENT_KEY = OpenSSL::PKey::RSA.new(File.open(CLIENT_KEY_PATH).read)
 CLIENT_CERT = OpenSSL::X509::Certificate.new(File.open(CLIENT_CERT_PATH).read)
