3b47563cdcc4a143d1464085d2673f2cf3dac298
Jeremy Ho <jujaga@gmail.com>
2013-09-03 16:07:16 -0700

    Removed some duplicate variable warnings
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- config/initializers/ssl_config.rb
+++ config/initializers/ssl_config.rb
@@ -9,8 +9,8 @@ SERVER_CERT_PATH_ = "cert/ca/LeadLab_root_cert_TEST.pem"
 CLIENT_KEY_PATH = "cert/ca/LeadLab_root_cert_TEST.pem"
 CLIENT_CERT_PATH = "cert/ca/LeadLab_root_cert_TEST.pem"
 #
-SERVER_KEY = OpenSSL::PKey::RSA.new(File.open(SERVER_KEY_PATH_).read)
-SERVER_CERT = OpenSSL::X509::Certificate.new(File.open(SERVER_CERT_PATH_).read)
+SERVER_KEY_ = OpenSSL::PKey::RSA.new(File.open(SERVER_KEY_PATH_).read)
+SERVER_CERT_ = OpenSSL::X509::Certificate.new(File.open(SERVER_CERT_PATH_).read)
 #
 CLIENT_KEY = OpenSSL::PKey::RSA.new(File.open(CLIENT_KEY_PATH).read)
 CLIENT_CERT = OpenSSL::X509::Certificate.new(File.open(CLIENT_CERT_PATH).read)
