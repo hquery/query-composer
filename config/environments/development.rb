538ab0194e7c3dbcb1f17490dcebdacc6fd8d343
Raymond Rusk <rusk.raymond@gmail.com>
2014-07-07 13:29:39 -0700

    Changed log level and added more codes to the code system helper

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ QueryComposer::Application.configure do
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
+  config.log_level = :info #:debug
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
