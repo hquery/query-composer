ee7a7785d5633ee8fe3b129b619b036c49d97e00
Raymond Rusk <rusk.raymond@gmail.com>
2015-04-20 09:53:54 -0700

    Modified for v2 queries

--- util/STOPP_report_tool.py
+++ util/STOPP_report_tool.py
@@ -3,13 +3,14 @@ __author__ = 'rrusk'
 import datetime
 import pymongo
 
+
 class MongoDatabase(object):
 
     DEFAULT_DB_NAME = "query_composer_development"
 
     ENDPOINTS_COLLECTION = "endpoints"
-    QUERIES_COLLECTION  = "queries"
-    RESULTS_COLLECTION  = "results"
+    QUERIES_COLLECTION = "queries"
+    RESULTS_COLLECTION = "results"
 
     def __init__(self, db_name=DEFAULT_DB_NAME, host="localhost", port=27017):
         self._client = pymongo.MongoClient("mongodb://{host}:{port}".format(host=host, port=port))
@@ -36,9 +37,10 @@ class MongoDatabase(object):
         find_query = {}
         return list(self._get_results_collection().find(find_query))
 
-    def get_query(self,queryid):
+    def get_query(self, queryid):
         find_query = {'_id': queryid}
         return (list(self._get_queries_collection().find(find_query)))[0]
+
 
 def select_result(execution_id, endpoint_id, results):
     for result in results:
@@ -46,37 +48,40 @@ def select_result(execution_id, endpoint_id, results):
             if result['endpoint_id'] == endpoint_id:
                 return result
 
+
 def main():
     db = MongoDatabase()
-    endpoints= db.get_endpoints()
+    endpoints = db.get_endpoints()
     queries = db.get_queries()
     results = db.get_results()
 
-    querySet = {}
+    query_set = {}
     for query in queries:
-        if not query['description'].startswith("STOPP Rule "):
+        if not query['description'].startswith("STOPP Rule ").endswith(' v2'):
             continue
-        querySet[query['description']] = query['_id']
-    queryDesc = sorted(querySet)
+        query_set[query['description']] = query['_id']
+    query_desc = sorted(query_set)
 
-    for desc in queryDesc:  # want queries sorted by description
-        queryid = querySet[desc]
+    for desc in query_desc:  # want queries sorted by description
+        queryid = query_set[desc]
         query = db.get_query(queryid)
         desc = query['description'].split()[2]
-        print desc, query['title']
+        print(str(desc), query['title'])
         executions = query['executions']
         for execution in executions:
             jstime = execution['time']
             dt = datetime.datetime.fromtimestamp(jstime)
-	    if dt < datetime.datetime(2015,2,19,12,0,0):
-		#print "dt =", dt
-		#print "date =", datetime.datetime(2015,2,19,12,0,0)
-		continue
+            # if dt < datetime.datetime(2015, 2, 19, 12, 0, 0):
+            if dt < datetime.datetime(2015, 4, 19, 22, 0, 0):
+                continue
+            # print "dt =", dt
+            # print "date =", datetime.datetime(2015,2,19,12,0,0)
             execution_id = execution['_id']
 
             try:
                 aggregate_result = execution['aggregate_result']
-            except KeyError: continue
+            except KeyError:
+                continue
             if aggregate_result:
                     keys = []
                     reportline = "  "
@@ -85,7 +90,7 @@ def main():
                     for key in aggregate_result:
                         reportline += ' '+str(key)+' '+str(int(aggregate_result[key]))
                         keys.append(key)
-                    print reportline
+                    print(reportline)
 
                     for endpoint in endpoints:
                         endpoint_id = endpoint['_id']
@@ -94,13 +99,16 @@ def main():
                             reportline = '    '+str(endpoint['name'])
                             try:
                                 value = result['value']
-                            except KeyError: continue
+                            except KeyError:
+                                continue
                             for key in keys:
                                 try:
-                                    reportline += ' '+ str(key)+' '+str(int(value[key]))
-                                except KeyError: continue
-                            print reportline
-        print
+                                    reportline += ' ' + str(key)+' '+str(int(value[key]))
+                                except KeyError:
+                                    continue
+                            print(reportline)
+        print()
                     
 
-if __name__ == '__main__':main()
+if __name__ == '__main__':
+    main()
