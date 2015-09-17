a5f780f2198893b414675262cc3a96bf1e4e8d02
Raymond Rusk <rusk.raymond@gmail.com>
2015-09-17 16:26:44 -0700

    More re-organization of Nagios config

--- monitoring/generate_nagios3_config.sh
+++ monitoring/generate_nagios3_config.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 # 
+# Endpoint ids (Oscar and Osler specified separately)
+ep_ids_oscar="0 1 2 3 4 5 6 7 8 9 11"
+ep_checks_oscar="diskspace import load processes swap tomcat users"
+ep_ids_osler=""
+ep_checks_osler="diskspace load processes swap users"
+
 print_info() {
-  echo "Plugins written into ./nagios-nrpe-server-config.  Move to /usr/local/lib/nagios once they"
-  echo "have been checked for correctness."
-  echo "NRPE commands written into ./nrpe_local.cfg.  Use this to replace"
-  echo "/etc/nagios/nrpe_local.cfg after carefully checking correctness."
-  echo "Reload nrpe plugins using 'sudo service nagios-nrpe-server reload'"
+  echo "Hub monitoring configuration:"
+  echo "NRPE commands written into ./nagios-nrpe-server-config/nrpe_local.cfg."
+  echo "Use this to replace /etc/nagios/nrpe_local.cfg after carefully checking correctness."
+  echo "Called plugins written into ./nagios-nrpe-server-config/plugins."
+  echo "Move to /usr/local/lib/nagios once they have been checked for correctness."
+  echo "Reload nrpe configuration using 'sudo service nagios-nrpe-server reload'"
 }
+
+/bin/rm -rf nagios-config && /bin/mkdir -p nagios-config
+/bin/rm -rf nagios-nrpe-server-config && /bin/mkdir -p nagios-nrpe-server-config/plugins
 
 service_description() {
   if [ "diskspace" = $1 ]; then 
@@ -29,7 +39,7 @@ service_description() {
 }
 
 generate_host_definition() {
-  ep_name=`/usr/bin/expr 1000 + $1 | cut -d1 -f2-`
+  ep_name=$1
   echo
   echo "# commands to check endpoint pdc-$ep_name"
   echo "define host {"
@@ -40,7 +50,7 @@ generate_host_definition() {
 }
 
 generate_host_service_definition() {
-  ep_name=`/usr/bin/expr 1000 + $1 | cut -d1 -f2-`
+  ep_name=$1
   echo "define service {"
   echo "	use		generic-service"
   echo "	host_name	pdc$ep_name"
@@ -50,7 +60,7 @@ generate_host_service_definition() {
 }
 
 generate_nagios_command_definition() {
-  ep_name=`/usr/bin/expr 1000 + $1 | cut -d1 -f2-`
+  ep_name=$1
   if [ "alive" = "$2" ]; then
     echo
     echo "# commands to check endpoint pdc-$ep_name"
@@ -67,60 +77,55 @@ generate_nagios_command_definition() {
 }
 
 generate_service_host_plugin() {
-  if [ ! -d ./nagios-nrpe-server-config ]; then
-    /bin/mkdir nagios-nrpe-server-config
-  fi
-  ep_name=`/usr/bin/expr 1000 + $1 | cut -d1 -f2-`
+  ep_name=$1
   ep_port=`/usr/bin/expr 10300 + $1`
-  /bin/sed "s/ep_port/$ep_port/;s/ep_check/$2/" ./check_service_pdc.template > ./nagios-nrpe-server-config/check_"$2"_"pdc$ep_name".sh
-  chmod +x ./nagios-nrpe-server-config/check_"$2"_"pdc$ep_name".sh
+  /bin/sed "s/ep_port/$ep_port/;s/ep_check/$2/" ./check_service_pdc.template > ./nagios-nrpe-server-config/plugins/check_"$2"_"pdc$ep_name".sh
+  chmod +x ./nagios-nrpe-server-config/plugins/check_"$2"_"pdc$ep_name".sh
 }
 
 generate_nrpe_command() {
-  ep_name=`/usr/bin/expr 1000 + $1 | cut -d1 -f2-`
+  ep_name=$1
   echo command[check_"$2"_"pdc$ep_name"]=/usr/local/lib/nagios/check_"$2"_"pdc$ep_name".sh
 }
 
 generate_alive_command() {
-  ep_name=`/usr/bin/expr 1000 + $1 | cut -d1 -f2-`
+  ep_name=$1
   ep_port=`/usr/bin/expr 10300 + $1`
   echo "# pdc$ep_name checks"
   echo command[check_alive_"pdc$ep_name"]=/usr/lib/nagios/plugins/check_http -I 127.0.0.1 -p $ep_port
   echo command[check_tunnel_"pdc$ep_name"]=/usr/lib/nagios/plugins/check_http -H localhost -p $ep_port
 }
 
-ep_ids_oscar="0 1 2 3 4 5 6 7 8 9 11"
-ep_checks_oscar="diskspace import load processes swap tomcat users"
-ep_ids_osler=""
-ep_checks_osler="diskspace load processes swap users"
 
-cp ./nrpe_local_cfg.prefix ./nrpe_local.cfg
-cp ./commands_cfg.prefix ./commands.cfg
+cp ./nrpe_local_cfg.prefix ./nagios-nrpe-server-config/nrpe_local.cfg
+cp ./commands_cfg.prefix ./nagios-config/commands.cfg
 
 # generate oscar configuration and plugins
 for id in $ep_ids_oscar
 do
-  #generate_host_definition $id >> ./commands.cfg
-  generate_nagios_command_definition $id "alive" >> ./commands.cfg
-  generate_alive_command $id >> ./nrpe_local.cfg
+  #generate_host_definition $id >> .//nagios-config/commands.cfg
+  ep_name=`/usr/bin/expr 1000 + $id | cut -d1 -f2-`
+  generate_nagios_command_definition $ep_name "alive" >> ./nagios-config/commands.cfg
+  generate_alive_command $ep_name >> ./nagios-nrpe-server-config/nrpe_local.cfg
   for check in $ep_checks_oscar
   do
-    generate_nagios_command_definition $id $check >> ./commands.cfg
-    generate_service_host_plugin $id $check # writes file directly
-    generate_nrpe_command $id $check >> ./nrpe_local.cfg
+    generate_nagios_command_definition $ep_name $check >> ./nagios-config/commands.cfg
+    generate_service_host_plugin $ep_name $check # writes file directly
+    generate_nrpe_command $ep_name $check >> ./nagios-nrpe-server-config/nrpe_local.cfg
   done
 done
 # generate osler configuration and plugins
 for id in $ep_ids_osler
 do
-  #generate_host_definition $id >> ./commands.cfg
-  generate_nagios_command_definition $id "alive" >> ./commands.cfg
-  generate_alive_command $id >> ./nrpe_local.cfg
+  #generate_host_definition $id >> .//nagios-config/commands.cfg
+  ep_name=`/usr/bin/expr 1000 + $id | cut -d1 -f2-`
+  generate_nagios_command_definition $ep_name "alive" >> ./nagios-config/commands.cfg
+  generate_alive_command $ep_name >> ./nagios-nrpe-server-config/nrpe_local.cfg
   for check in $ep_checks_osler
   do
-    generate_nagios_command_definition $id $check >> ./commands.cfg
-    generate_service_host_plugin $id $check # writes file directly
-    generate_nrpe_command $id $check >> ./nrpe_local.cfg
+    generate_nagios_command_definition $ep_name $check >> ./nagios-config/commands.cfg
+    generate_service_host_plugin $ep_name $check # writes file directly
+    generate_nrpe_command $ep_name $check >> ./nagios-nrpe-server-config/nrpe_local.cfg
   done
 done
 
